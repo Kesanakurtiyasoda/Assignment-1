a3b595b85e27807248d795a1f851b07201158fae
kesanakurti yasoda <yashoda2000.k@gmail.com>
2022-01-27 22:20:58 +0530

    final

---
+++ intro.js
@@ -0,0 +1,73 @@
+// assignment-1
+var name = "yashoda";
+
+console.log(name)
+
+var age = 21;
+
+console.log(age)
+
+var married = false
+
+console.log(married)
+// assignment-2
+var gender = "female";
+
+console.log(gender)
+
+var height = 57.6;
+
+console.log(height)
+
+var nonveg = true
+
+console.log(nonveg)
+// assignment-3
+var currentyear = 2022;
+var yearofbirth= 2000;
+
+console.log(currentyear - yearofbirth);
+// assignment-4
+var maths = 10;
+var physics = 15;
+var english = 20;
+
+console.log(maths + physics + english);
+// assignment-5
+var width = 8;
+var length = 12;
+
+console.log(length * width);
+console.log(2* (length + width));
+// assignment-6
+var totalmonths = 50;
+var totalmonthstoyear = 48
+
+console.log(totalmonths - totalmonthstoyear);
+// assignment-7
+var principalamount = 1000;
+var interest = 5;
+var time = 2;
+
+console.log(principalamount * interest * time /(100));
+// assignment-8
+var student1 = 15;
+var student2 = 10;
+var student3 = 12;
+var student4 = 11;
+var student5 = 14;
+var student6 = 9;
+
+console.log((student1+student2+student3+student4+student5+student6)/(6));
+// assignment-9
+var product1 = 10*2;
+var product2 = 15*4;
+var product3 = 20*5;
+
+console.log(product1 + product2 + product3);
+// assignment-10
+var item1 = (50*1)+4-5+6;
+var item2 = (100*2)+6+10+8;
+var item3 = (150*3)+8+20+10;
+
+console.log(item1 + item2 + item3);
